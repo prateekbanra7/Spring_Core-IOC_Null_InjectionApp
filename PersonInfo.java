d98e54be1136027f10c36b483c304c0076888272
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-05-19 17:51:52 +0530

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,29 @@
+package in.abc.bean;
+
+import java.util.Date;
+
+public class PersonInfo {
+
+	private Long pno;
+	private String pname;
+	
+	private Date dob;
+	private Date doj;
+	private Date dom;
+	
+	public PersonInfo(Long pno, String pname, Date dob, Date doj, Date dom) {
+		System.out.println("PersonInfo:: 5 param constructor...");
+		this.pno = pno;
+		this.pname = pname;
+		this.dob = dob;
+		this.doj = doj;
+		this.dom = dom;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonInfo [pno=" + pno + ", pname=" + pname + ", dob=" + dob + ", doj=" + doj + ", dom=" + dom + "]";
+	}
+	
+	
+}
